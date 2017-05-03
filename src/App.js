e442167564af660ccdcc2f4a7a2d0f7181fdc639
Anna Kartynnik <AKartynnik@qulix.com>
2017-05-03 14:37:51 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Languages from 'components/Languages';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Languages />
+      </div>
+    );
+  }
+}
+
+export default App;
